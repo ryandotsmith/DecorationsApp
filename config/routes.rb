3f0c4e2ccfab87293ea32749a3bce15e223fa041
ryandotsmith <this.ryansmith@gmail.com>
2009-11-19 08:28:25 -0800

    added haml and a todo_lists controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  map.root :controller => 'sessions',:action => 'new'
+  map.root :controller => 'todo_lists',:action => 'index'
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end
---
+++ spec/controllers/todo_lists_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe TodoListsController do
+
+  #Delete this example and add some real ones
+  it "should use TodoListsController" do
+    controller.should be_an_instance_of(TodoListsController)
+  end
+
+end
