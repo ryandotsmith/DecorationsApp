de1c31a9d2e9e4872fae82be78381f7b310cef50
ryandotsmith <this.ryansmith@gmail.com>
2009-11-22 20:37:13 -0800

    added some files for the basecamp accounts controller

---
+++ spec/controllers/basecamp_accounts_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe BasecampAccountsController do
+
+  #Delete this example and add some real ones
+  it "should use BasecampAccountsController" do
+    controller.should be_an_instance_of(BasecampAccountsController)
+  end
+
+end
